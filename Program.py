d13cf0f477155663f6e2cc394c9c1c5799a9fbc4
MamaevEA <mamaevea@gmail.com>
2022-12-07 22:11:52 +0500

    Add functhion Transform files

--- Program.py
+++ Program.py
@@ -1,27 +1,25 @@
 import pandas as pd
+from pathlib import *
 
 def transform_MD(MD):
     MD = '-'+MD
     return MD
 
-df = pd.read_csv('TRJ/22ПЛ.txt', encoding='ISO-8859-1')
+df = pd.read_table('TRJ/22ПЛ.txt', sep='/s*', engine='python', encoding='cp1251')
 name = df['#  X  Y  TVDSS  MD'][0]
-name = name.split()
-name = {'X': name[0], 'Y': name[1], 'TVDSS': '', 'MD': ''}
-name = pd.DataFrame([name])
 df.drop(labels = [0],axis = 0, inplace=True)
 df = df['#  X  Y  TVDSS  MD'].str.split(expand=True)
 df.columns=['X', 'Y', 'TVDSS', 'MD']
 df['MD'] = df['MD'].apply(transform_MD)
-df = pd.DataFrame(df)
-res = pd.concat([name, df], ignore_index=True)
-
 
 with open('TRJ/22ПЛ.txt', 'w') as file:
-    file.write(res.to_string(index=False))
+    file.write(f'#  X  Y  TVDSS  MD \n {name} \n')
+
+with open('TRJ/22ПЛ.txt', 'a') as file:
+    file.write(df.to_string(index=False, header=None, col_space=12))
+
 
 
 print(df.info())
 print(df)
 print(name)
-print(res)
