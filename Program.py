8a66b14d4f8062f3ebda2132bc9d9b9c4490c355
MamaevEA <mamaevea@gmail.com>
2022-12-08 14:54:35 +0500

    Add EXE-file

--- Program.py
+++ Program.py
@@ -1,25 +1,28 @@
 import pandas as pd
 from pathlib import *
+from easygui import *
 
 def transform_MD(MD):
     MD = '-'+MD
     return MD
+def transform_file():
+    df = pd.read_table(f'{fieldValues[i]}', sep='/s*', engine='python', encoding='cp1251')
+    name = df['#  X  Y  TVDSS  MD'][0]
+    df.drop(labels = [0],axis = 0, inplace=True)
+    df = df['#  X  Y  TVDSS  MD'].str.split(expand=True)
+    df.columns=['X', 'Y', 'TVDSS', 'MD']
+    df['MD'] = df['MD'].apply(transform_MD)
+    with open(f'{fieldValues[i]}', 'w') as file:
+        file.write(f'#  X  Y  TVDSS  MD \n {name} \n')
+        file.write(df.to_string(index=False, header=None, col_space=12))
 
-df = pd.read_table('TRJ/22ПЛ.txt', sep='/s*', engine='python', encoding='cp1251')
-name = df['#  X  Y  TVDSS  MD'][0]
-df.drop(labels = [0],axis = 0, inplace=True)
-df = df['#  X  Y  TVDSS  MD'].str.split(expand=True)
-df.columns=['X', 'Y', 'TVDSS', 'MD']
-df['MD'] = df['MD'].apply(transform_MD)
+msg = "Уважаемый пользователь! \n \n \
+    Данная программа редактирует .txt файлы, \n \
+    добавляя впереди чисел столбца 'MD' знак '-'. \n \n \
+    Выберите файлы для редактирования."
+title = "Выбор файлов для редактирования"
+txt_files = list(Path.cwd().glob('*.txt'))
+fieldValues = multchoicebox(msg,title, txt_files)
 
-with open('TRJ/22ПЛ.txt', 'w') as file:
-    file.write(f'#  X  Y  TVDSS  MD \n {name} \n')
-
-with open('TRJ/22ПЛ.txt', 'a') as file:
-    file.write(df.to_string(index=False, header=None, col_space=12))
-
-
-
-print(df.info())
-print(df)
-print(name)
+for i in range(len(txt_files)):
+    transform_file()
